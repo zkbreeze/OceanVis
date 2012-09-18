142151b32472b4f9337564c6e1b5f4c6c9bac531
Kun Zhao <zk.kyoto@gmail.com>
2012-09-18 21:19:38 +0900

    revise

--- compression/peel/main.cpp
+++ compression/peel/main.cpp
@@ -45,8 +45,6 @@ int main( int argc, char** argv )
                 size_t index = i + j * nx_new + k * nx_new * ny_new;
                 size_t index_ori = ( i + 1 ) + ( j + 1 ) * nx + ( k + 1 ) * nx * ny;
                 buf[index] = pvalues[index_ori];
-                std::cout << index << std::endl;
-                std::cout << index_ori << std::endl;
             }
 
     
