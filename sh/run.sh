d0937351f1d054d6cc36d19bc465a65bdcd26669
Kun Zhao <zk.kyoto@gmail.com>
2012-09-23 17:26:24 +0900

    ok

--- sh/run.sh
+++ sh/run.sh
@@ -7,7 +7,7 @@ do
     t=0
     while [ $t -ne 8 ]
     do
-./preProcess -s rm_rokka19_200803${d}__s_000$t.kvsml -outname 200803${d}_s_000$t.kvsml
+./preProcess -s rm_rokka19_200803${d}__s_000$t.kvsml -outname pre/200803${d}_s_000$t.kvsml
     t=`expr $t + 1`
     done
 d=`expr $d + 1`
@@ -20,7 +20,7 @@ do
     t=0
     while [ $t -ne 8 ]
     do
-./compresion -f 200803${d}_s_000$t.kvsml -t 0315s.kvsml -sp 3 -PBVR -Bspline -b 2 -outpoint s_$step.kvsml
+./compresion -f pre/200803${d}_s_000$t.kvsml -t 0315s.kvsml -sp 3 -PBVR -Bspline -b 2 -outpoint PBVR/s_$step.kvsml
     t=`expr $t + 1`
     step=`expr $step + 1`
     done
